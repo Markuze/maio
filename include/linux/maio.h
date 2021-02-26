0617e0a284f43582d2bd7d64ccfd6d41bf8f4a9b
Markuze Alex <amarkuze@vmware.com>
2021-02-26 15:10:17 +0000

    MAIO Multiple dev support

--- include/linux/maio.h
+++ include/linux/maio.h
@@ -12,10 +12,13 @@
 #define PAGES_IN_HUGE	(1<<HUGE_ORDER)
 
 #define NUM_MAX_RINGS	16
+#define MAX_DEV_NUM 	16
 
 #if 0
+//TODO: Take these at init and change to __read_mostly var
+#define UMAIO_STRIDE		0x1000
+#define UMAIO_STRIDE_MASK	(0x1000-1)
 #define UMAIO_HEADROOM	256 	//TODO: Figure out why 256 results in 4K stride in mlx5e
-#define UMAIO_STRIDE	0x1000
 #define UMAIO_RING_SZ	512
 #define UMAIO_RING_MASK	(UMAIO_RING_SZ -1)
 #endif
@@ -26,7 +29,7 @@ typedef bool (*maio_filter_func_p)(void *);
 
 extern maio_filter_func_p maio_filter;
 extern volatile bool maio_configured;
-extern struct user_matrix *global_maio_matrix;
+extern struct user_matrix *global_maio_matrix[MAX_DEV_NUM];
 
 /********* Caution: Should be same as user counterpart ************************/
 
@@ -95,10 +98,21 @@ struct percpu_maio_qp {
 	void *cached_mbuf;
 };
 
+struct percpu_maio_dev_qp {
+	struct percpu_maio_qp qp[MAX_DEV_NUM];
+};
+
+/* on RX use the mtrx of the upper dev */
+/* on TX use the netdev of the lower(i.e., slave) dev */
+struct maio_dev_map {
+	int on_tx[MAX_DEV_NUM];
+	int on_rx[MAX_DEV_NUM];
+};
+
 void reset_maio_default_filter(void);
 u16 maio_get_page_headroom(struct page *page);
-int maio_post_rx_page(void *addr, u32 len);
-int maio_post_rx_page_copy(void *addr, u32 len);
+int maio_post_rx_page(struct net_device *net, void *addr, u32 len);
+int maio_post_rx_page_copy(struct net_device *net, void *addr, u32 len);
 void maio_frag_free(void *addr);
 void maio_page_free(struct page *page);
 void *maio_kalloc(void);
